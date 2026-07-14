d176e451895c18e1f29373ba0956cfeac74b8d05
Rie <3981784822@qq.com>
2026-07-14 08:42:55 +0800

    Simple Comparison Test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,19 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> normallist = new AListNoResizing<>();
+        BuggyAList<Integer> buggylist = new BuggyAList<>();
+        for (int i = 0; i < 3; i++){
+            normallist.addLast(i);
+            buggylist.addLast(i);
+        }
+        for (int i = 0; i < 3; i++){
+            assertEquals(normallist.removeLast(), buggylist.removeLast());
+        }
+
+    }
+
 }
+
