07857d2214598342ceaf02655d4800997bfa1900
Rie <3981784822@qq.com>
2026-07-14 09:43:59 +0800

    Adding Randomized Comparisons

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -26,29 +26,37 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest(){
         AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> L2 = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
+                L2.addLast(randVal);
+//                System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
                 int size = L.size();
-                System.out.println("size: " + size);
+                int size2 = L2.size();
+                assertEquals(size, size2);
+//                System.out.println("size: " + size);
             } else if (operationNumber == 2) {
                 //getLast
                 if(L.size() == 0)   continue;
                 int num = L.getLast();
-                System.out.println("last number: " + num);
+                int num2 = L2.getLast();
+                assertEquals(num, num2);
+//                System.out.println("last number: " + num);
             } else if (operationNumber == 3) {
                 //removeLast
                 if(L.size() == 0)   continue;
                 int num = L.removeLast();
-                System.out.println("remove: " + num);
+                int num2 = L2.removeLast();
+                assertEquals(num,num2);
+//                System.out.println("remove: " + num);
             }
         }
     }
