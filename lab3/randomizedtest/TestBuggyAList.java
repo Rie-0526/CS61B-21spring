86596e96b82f850af84a88e16a1e5de4f8e15362
Rie <3981784822@qq.com>
2026-07-14 09:19:49 +0800

    Randomized Function Calls

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -20,8 +20,27 @@ public class TestBuggyAList {
         for (int i = 0; i < 3; i++){
             assertEquals(normallist.removeLast(), buggylist.removeLast());
         }
-
     }
 
+    //don't be written by student
+    @Test
+    public void randomizedTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 2);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            }
+        }
+    }
 }
 
