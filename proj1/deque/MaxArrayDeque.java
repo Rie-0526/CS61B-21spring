daf429cfc3d2266dad8688e3af589d22f0382e93
Rie <3981784822@qq.com>
2026-07-19 18:59:59 +0800

    Remove redundant code in MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,14 +1,8 @@
 package deque;
 
-public class MaxArrayDeque<T> {
-    // 规定：当size == 0时，firstIndex == lastIndex
-    int size = 0;
-    int firstIndex = 0;
-    int lastIndex = 0;
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+
     Comparator<T> comparator;
-
-    T[] array = (T[]) new Object[8];
-
 
     public MaxArrayDeque(Comparator<T> c){
         comparator = c;
@@ -17,7 +11,7 @@ public class MaxArrayDeque<T> {
     public T max(Comparator<T> c) {
         T mostItem = get(0);
         int cmpResult;
-        for (int i = 1; i < size; i++){
+        for (int i = 1; i < size(); i++){
             cmpResult = c.compare(get(i), mostItem);
             if (cmpResult > 0) {
                 mostItem = get(i);
@@ -30,149 +24,4 @@ public class MaxArrayDeque<T> {
         return max(comparator);
     }
 
-
-    private void resizeArray(int capacity){
-        T[] temp = (T[]) new Object[capacity];
-        // Copy the array
-        int j = 0;
-        if (firstIndex <= lastIndex){
-            for(int i = firstIndex; i <= lastIndex; i++){
-                temp[j] = array[i];
-                j++;
-            }
-        }
-        else {
-            for (int i = firstIndex; i < array.length; i++){
-                temp[j] = array[i];
-                j++;
-            }
-            for (int i = 0; i <= lastIndex; i++){
-                temp[j] = array[i];
-                j++;
-            }
-        }
-
-        array = temp;
-        firstIndex = 0;
-        lastIndex = j - 1;
-
-    }
-
-
-    public void addFirst(T item){
-        if(size == array.length){
-            resizeArray(size * 2);
-        }
-        if (size != 0){
-            --firstIndex;
-            if (firstIndex < 0) {
-                firstIndex = array.length - 1;
-            }
-        }
-        array[firstIndex] = item;
-        size++;
-    }
-
-    public void addLast(T item){
-        if(size == array.length){
-            resizeArray(size * 2);
-        }
-        if (size != 0) {
-            ++lastIndex;
-            if (lastIndex >= array.length) {
-                lastIndex = 0;
-            }
-        }
-        array[lastIndex] = item;
-        size++;
-    }
-
-    // 感觉最好写一个firstIndex/lastIndex增加/减少方法
-    // 你这不纯迭代器的超级无敌劣质low版本
-
-    public boolean isEmpty(){
-        if (size == 0)  return true;
-        else return false;
-    }
-
-    public int size(){
-        return size;
-    }
-
-    public void printDeque(){
-        if (size == 0)  {
-            System.out.println();
-            return;
-        }
-
-        if (firstIndex <= lastIndex){
-            for(int i = firstIndex; i <= lastIndex; i++){
-                System.out.println(array[i]);
-            }
-        }
-        else {
-            for (int i = firstIndex; i < array.length; i++){
-                System.out.println(array[i]);
-            }
-            for (int i = 0; i <= lastIndex; i++){
-                System.out.println(array[i]);
-            }
-        }
-    }
-
-    public T removeFirst(){
-        if (size == 0)  {return null;}
-
-        T removeValue = array[firstIndex];
-        array[firstIndex] = null;
-
-        if (size > 1){
-            firstIndex++;
-            if (firstIndex > array.length - 1) {
-                firstIndex = 0;
-            }
-        }
-
-        size--;
-
-        if (size < array.length / 4){
-            resizeArray(array.length / 4);
-        }
-
-        return removeValue;
-    }
-
-    public T removeLast(){
-        if (size == 0)  {return null;}
-
-        T removeValue = array[lastIndex];
-        array[lastIndex] = null;
-
-        if (size > 1){
-            lastIndex--;
-            if (lastIndex < 0) {
-                lastIndex = array.length - 1;
-            }
-        }
-
-        size--;
-
-        if (size < array.length / 4){
-            resizeArray(array.length / 4);
-        }
-
-        return removeValue;
-    }
-
-    public T get(int index){
-
-        if (index >= size)  {return null;}
-
-        int realIndex = index + firstIndex;
-        if (realIndex >= array.length){
-            realIndex -= array.length;
-        }
-        return array[realIndex];
-
-    }
 }
