e03f07131f2c9582fbb9ec53446ea04e9fa91c3e
Rie <3981784822@qq.com>
2026-07-24 15:13:40 +0800

    Finished: init() in proj2.

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,7 +2,10 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -10,7 +13,7 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -22,5 +25,38 @@ public class Commit {
     /** The message of this Commit. */
     private String message;
 
+    /** The time of this Commit. */
+    private Instant time;
+
+    /** The hash value(commit file name) of parents of this Commit. */
+    private ArrayList<String> parents;
+
+    /** The map of file names to blob references */
+    private HashMap<String, String> fileMap;
+
     /* TODO: fill in the rest of this class. */
+    public void setMessage(String text){
+        message = text;
+    }
+
+    public void setNowTime(){
+        time = Instant.now();
+    }
+
+    public void setEpochTime(){
+        time = Instant.ofEpochSecond(0);
+    }
+
+    public void setParents(ArrayList<String> p){
+        for (String i : p) {
+            parents.add(i);
+        }
+    }
+
+
+
+
+
+
+
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,8 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -25,5 +27,56 @@ public class Repository {
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
+    /** The object directory. */
+    public static final File OBJECT_DIR = join(CWD, "object");
+    /** The commits directory. */
+    public static final File COMMITS_DIR = join(CWD, "commits");
+    /** The storing area directory */
+    public static final File STORING_AREA = join(CWD, "storingArea");
+
+    public static final File HEAD = join(CWD, "HEAD");
+    /** The branch directory */
+    public static final File BRANCH = join(CWD, "branch");
+    /** The master file of branch directory */
+    public static final File master = join(BRANCH, "master");
+
+
     /* TODO: fill in the rest of this class. */
+    public void setGitletDir() {
+        GITLET_DIR.mkdir();
+        OBJECT_DIR.mkdir();
+        COMMITS_DIR.mkdir();
+        STORING_AREA.mkdir();
+        BRANCH.mkdir();
+    }
+
+
+
+    public void init() throws IOException {
+
+        Commit initialCommit = new Commit();
+        initialCommit.setMessage("initial commit");
+        initialCommit.setEpochTime();
+        initialCommit.setParents(null);
+
+        String ICfileName = sha1(initialCommit);
+        File ICfile = join(COMMITS_DIR, ICfileName);
+
+
+        if(ICfile.exists() && HEAD.exists() && master.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+
+        setGitletDir();
+        ICfile.createNewFile();
+        Utils.writeObject(ICfile, initialCommit);
+        master.createNewFile();
+        Utils.writeContents(master, ICfileName);
+        HEAD.createNewFile();
+        Utils.writeContents(HEAD, master.getPath());
+
+    }
+
+
 }
